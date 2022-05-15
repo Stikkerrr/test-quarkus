6123bf76e5500d966691704d36d5c1a92ccaffe2
quarkusio <no-reply@quarkus.io>
2022-05-15 10:38:42 +0000

    Initial commit

---
+++ src/native-test/java/org/stikkerrr/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.stikkerrr;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
